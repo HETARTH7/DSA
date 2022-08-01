057c7c7958d9374ba1ca91c167b6344680b28421
unknown <hetarth002@gmail.com>
2022-08-02 05:04:11 +0530

    singly linked list delHead

--- linkedList.cpp
+++ linkedList.cpp
@@ -20,17 +20,51 @@ void Traverse(Node *head)
         head = head->next;
     }
 };
-Node *insertAtTheBeginning(Node *head, int x)
+Node *insertBeginning(Node *head, int x)
 {
     Node *temp = new Node(x);
     temp->next = head;
     return temp;
 }
+Node *insertEnd(Node *head, int x)
+{
+    Node *temp = new Node(x);
+    if (head == NULL)
+    {
+        return temp;
+    }
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    return head;
+}
+Node *delHead(Node *head)
+{
+    if (head = NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        Node *temp = head->next;
+        delete head;
+        return temp;
+    }
+}
 int main()
 {
     Node *head = NULL;
-    head = insertAtTheBeginning(head, 10);
-    head = insertAtTheBeginning(head, 20);
-    head = insertAtTheBeginning(head, 30);
+    head = insertBeginning(head, 30);
+    head = insertBeginning(head, 20);
+    head = insertBeginning(head, 10);
+    Traverse(head);
+    cout << endl;
+    head = insertEnd(head, 40);
+    Traverse(head);
+    cout << endl;
+    head = delHead(head);
     Traverse(head);
 }
