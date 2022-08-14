1e485a56441480a62195b19f8e89306e514b50ac
unknown <hetarth002@gmail.com>
2022-08-14 12:02:43 +0530

    merge

---
+++ mergeSort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int a[], int b[], int m, int n)
+{
+    int i = 0, j = 0;
+    while (i < m && j < n)
+    {
+        if (a[i] <= b[j])
+        {
+            cout << a[i] << " ";
+            i++;
+        }
+        else
+        {
+            cout << b[j] << " ";
+            j++;
+        }
+    }
+    while (i < m)
+    {
+        cout << a[i] << " ";
+        i++;
+    }
+    while (j < n)
+    {
+        cout << b[j] << " ";
+        j++;
+    }
+}
+int main()
+{
+    int a[3] = {10, 20, 50};
+    int b[3] = {5, 50, 50};
+    merge(a, b, 3, 3);
+}
