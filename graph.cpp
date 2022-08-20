9ed07e412d876c63feb5b3b382bdcc2bc764d516
unknown <hetarth002@gmail.com>
2022-08-20 19:13:45 +0530

    graph implemtation

---
+++ graph.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<int> adj[], int v, int u)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printGraph(vector<int> adj[], int v)
+{
+    for (int i = 0; i < v; i++)
+    {
+        for (int x : adj[i])
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int v;
+    vector<int> adj[v];
+}
