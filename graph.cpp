cdc546cb8545c1d039b31d7ffd2f99fb966a6ee2
unknown <hetarth002@gmail.com>
2022-09-08 10:58:47 +0530

    graph bfs

--- graph.cpp
+++ graph.cpp
@@ -18,6 +18,32 @@ void printGraph(vector<int> adj[], int v)
         cout << endl;
     }
 }
+
+void bfs(vector<int> adj[], int v, int s)
+{
+    bool visited[v + 1];
+    for (int i = 0; i < v; i++)
+    {
+        visited[i] = false;
+    }
+    queue<int> q;
+    visited[s] = true;
+    q.push(s);
+    while (q.empty() == false)
+    {
+        int u = q.front();
+        q.pop();
+        cout << u << " ";
+        for (int v : adj[u])
+        {
+            if (visited[v] == false)
+            {
+                visited[v] = true;
+                q.push(v);
+            }
+        }
+    }
+}
 int main()
 {
     int v;
