dbda2c2f53600a070c1489bb83fe532292c1cded
Hetarth <hetarth2028@gmail.com>
2025-04-26 17:48:20 +0530

    tree

---
+++ tree.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+# Time Complexity: O(n)
+def preorder(root):
+    if not root:
+        return None
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+# Time Complexity: O(n)
+def inorder(root):
+    if not root:
+        return None
+    inorder(root.left)
+    print(root.val)
+    inorder(root.right)
+
+# Time Complexity: O(n)
+def postorder(root):
+    if not root:
+        return None
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Inorder traversal ")
+inorder(root)
+
+print("\nPreorder traversal ")
+preorder(root)
+
+print("\nPostorder traversal ")
+postorder(root)
